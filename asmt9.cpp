22cfc848c8c94a27bd52bef33f0d8e48c91fb2f4
VedantGaikwad123 <169071599+VedantGaikwad123@users.noreply.github.com>
2025-05-10 09:26:37 +0530

    Update asmt9.cpp

--- asmt9.cpp
+++ asmt9.cpp
@@ -1,53 +1,53 @@
 #include<iostream>
 using namespace std;
 
-class heaptree{
-    public:
+class heaptree {
+public:
     int totalkeys;
-    int value;
     int heap[10];
 
-    void insert(){
-        cout<<"Enter total number of keys: ";
-        cin>>totalkeys;
-        for(int loc=0;loc<totalkeys;loc++){
-            cout<<"Enter key: ";
-            cin>>value;
-            built(loc,totalkeys,heap);
+    void insert() {
+        cout << "Enter total number of keys (max 10): ";
+        cin >> totalkeys;
+        for (int loc = 0; loc < totalkeys; loc++) {
+            int value;
+            cout << "Enter key: ";
+            cin >> value;
+            heap[loc] = value;
+            built(loc);
         }
     }
 
-    void built(int loc,int totalkeys,int heap[]){
-        int parent = loc/2;
-        while (heap[parent] > heap[loc]){
-            int temp = heap[parent];
-            heap[parent] = heap[loc];
-            heap[loc] = temp;
+    void built(int loc) {
+        int parent = (loc - 1) / 2;
+        while (loc > 0 && heap[parent] > heap[loc]) {
+            swap(heap[parent], heap[loc]);
             loc = parent;
+            parent = (loc - 1) / 2;
         }
     }
 
-    void display(){
-        cout<<"Heap Tree: ";
-        for(int loc=0;loc<totalkeys;loc++){
-            cout<<heap[loc]<<" ";
+    void display() {
+        cout << "Heap Tree: ";
+        for (int loc = 0; loc < totalkeys; loc++) {
+            cout << heap[loc] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-}heapObject;
+} heapObject;
 
-int main(){
+int main() {
     int choice;
-    do{
-        cout<<"1. Accept Data \t 2. Display Heap \t 0. Exit Program";
-        cout<<"Enter the Choice : ";
-        cin>>choice;
-        switch(choice){
+    do {
+        cout << "\n1. Accept Data \t 2. Display Heap \t 0. Exit Program\n";
+        cout << "Enter the Choice : ";
+        cin >> choice;
+        switch (choice) {
             case 1: heapObject.insert(); break;
             case 2: heapObject.display(); break;
-            case 0: cout<<"Exiting program..."; break;
-            default: cout<<"Invalid choice! Please try again."; break;
+            case 0: cout << "Exiting program...\n"; break;
+            default: cout << "Invalid choice! Please try again.\n"; break;
         }
-    }while(choice != 0);
+    } while (choice != 0);
     return 0;
 }
